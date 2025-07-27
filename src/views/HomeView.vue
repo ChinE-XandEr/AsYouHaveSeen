a1ee1e4fe182b5233ffdd5e60343d8855bdc8fe4
ChinE_XandEr <yanzuo_cheng@outlook.com>
2025-07-27 13:18:35 +0800

    https://github.com/ChinE-XandEr/AsYouHaveSeen.git

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,48 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>我的第一个 Vue 应用</span>
+        </div>
+      </template>
+      <el-button type="primary" @click="handleClick">
+        <el-icon><Plus /></el-icon>
+        点击我
+      </el-button>
+      <p>{{ message }}</p>
+    </el-card>
+  </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const message = ref('Hello Vue + Element Plus!')
+
+    const handleClick = () => {
+      message.value = '按钮被点击了！'
+    }
+
+    return {
+      message,
+      handleClick
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+.box-card {
+  max-width: 480px;
+  margin: 0 auto;
+}
+</style>
